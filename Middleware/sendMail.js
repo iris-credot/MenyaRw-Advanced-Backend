4ecf5f8ac7e6b1e6bdddb9a93d386cdfe2abfefd
Iris Credot TETA <137190728+iris-credot@users.noreply.github.com>
2026-05-18 16:01:12 +0200

    Add TLS configuration to sendMail middleware

--- Middleware/sendMail.js
+++ Middleware/sendMail.js
@@ -8,6 +8,9 @@ const transporter = nodemailer.createTransport({
     user: process.env.AUTH_EMAIL,
     pass: process.env.AUTH_PASS,
   },
+   tls: {
+    rejectUnauthorized: false,
+  },
   family: 4,
 });
 
