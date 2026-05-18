5eff35e88a35497c713a4c21ba250f77d39e7daf
Iris Credot TETA <137190728+iris-credot@users.noreply.github.com>
2026-05-18 15:51:25 +0200

    Refactor sendMail.js for improved error handling

--- Middleware/sendMail.js
+++ Middleware/sendMail.js
@@ -8,25 +8,26 @@ const transporter = nodemailer.createTransport({
     user: process.env.AUTH_EMAIL,
     pass: process.env.AUTH_PASS,
   },
-  tls: {
-    rejectUnauthorized: false,
-  },
   family: 4,
 });
 
 const sendEmail = async (to, subject, body) => {
   try {
+    await transporter.verify();
+    console.log('SMTP ready');
+
     const info = await transporter.sendMail({
-      from: `"Menya-Rwanda" <${process.env.AUTH_EMAIL}>`,
+      from: `"Menya Rwanda" <${process.env.AUTH_EMAIL}>`,
       to,
       subject,
       text: body,
     });
 
     console.log('Email sent:', info.response);
+
   } catch (error) {
     console.error('FULL EMAIL ERROR:', error);
-    throw new Error('Failed to send email');
+    throw error;
   }
 };
 
